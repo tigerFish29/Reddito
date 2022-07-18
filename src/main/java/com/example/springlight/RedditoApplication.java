a34a83b1a472d68821c43aca1f9ad019553fa935
tigerFish29 <paulwchabz28@gmail.com>
2022-07-18 20:45:52 +0100

    First initial commit of the project

---
+++ src/main/java/com/example/springlight/RedditoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springlight/RedditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
