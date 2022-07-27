dad4027ac633863b5a41ac46ce98f21df66710c3
tigerFish29 <paulwchabz28@gmail.com>
2022-07-27 01:03:27 +0100

    created our entities for the project

---
+++ src/main/java/com/example/springlight/domain/Vote.java
@@ -0,0 +1,23 @@
+package com.example.springlight.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Vote {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+
+    private int vote;
+
+
+}
